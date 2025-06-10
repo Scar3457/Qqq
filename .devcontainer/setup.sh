3605a0497233d1b2572603879463408b3a4fc39d
Scar3457 <61478911+Scar3457@users.noreply.github.com>
2025-06-10 09:54:56 +0300

    Update and rename post-create.sh to setup.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+mkdir -p ~/dockercom
+cd ~/dockercom || exit 1
+
+cat > docker-compose.yml <<EOF
+version: '3.8'
+services:
+  ubuntu-gui:
+    image: dorowu/ubuntu-desktop-lxde-vnc:bionic
+    container_name: ubuntu_gui
+    ports:
+      - "6080:80"
+      - "5900:5900"
+    environment:
+      - VNC_PASSWORD=pass123
+    volumes:
+      - ./data:/data
+      - /dev/net/tun:/dev/net/tun
+    cap_add:
+      - NET_ADMIN
+    devices:
+      - /dev/net/tun
+    privileged: true
+    shm_size: "2g"
+EOF
+
+sudo docker-compose up -d
+
+sleep 5
+
+sudo docker exec ubuntu_gui bash -c \"\
+  apt update && \
+  apt install -y openvpn curl && \
+  cd /tmp && \
+  curl -L -o vpn.ovpn https://raw.githubusercontent.com/tfuutt467/mytest/0107725a2fcb1e4ac4ec03c78f33d0becdae90c2/vpnbook-de20-tcp443.ovpn && \
+  echo -e 'vpnbook\ncf32e5w' > auth.txt && \
+  openvpn --config vpn.ovpn --auth-user-pass auth.txt --daemon\"
